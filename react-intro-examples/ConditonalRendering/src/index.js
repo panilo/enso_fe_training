2a214a21e2ebcbd2bc17b5cc07f168dc74c43b50
Danilo Desole <danilodesole@gmail.com>
2021-09-29 15:37:54 +0100

    Adding conditional rendering

---
+++ react-intro-examples/ConditonalRendering/src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import "./index.css";
+import LoginController from "./components/LoginController";
+
+const loginController = <LoginController />;
+
+ReactDOM.render(loginController, document.getElementById("root"));
