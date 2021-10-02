c5a43eefb05429b71bcc57d64fcc9bf4e1d03853
Danilo Desole <danilodesole@gmail.com>
2021-10-02 15:38:36 +0100

    Minor fix

--- react-intro-examples/HookAndEffect/src/context/ContextProviderHello.js
+++ react-intro-examples/HookAndEffect/src/context/ContextProviderHello.js
@@ -1,23 +1,23 @@
 import React, { useContext, createContext } from "react";
-import { useHello } from "../hooks/useHello";
+import { useSayHello } from "../hooks/useSayHello";
 
 // This is a context creation: context will allow to provide a state for children component
-const helloContext = createContext();
+const sayHelloContext = createContext();
 
 // In order to share the state we need a context, a state-wrapper for components.
 // This component will create the context provider for children component
 // letting us use the custom hook useHello and will make possible to share
 // the state among all the children components.
 export function ContextProviderHello(props) {
-  const helloHook = useHello();
+  const sayHelloHook = useSayHello();
   return (
-    <helloContext.Provider value={helloHook}>
+    <sayHelloContext.Provider value={sayHelloHook}>
       {props.children}
-    </helloContext.Provider>
+    </sayHelloContext.Provider>
   );
 }
 
 // This function is an hook and will be used in children components to get the latest context value (latest state)
 export function useContextHello() {
-  return useContext(helloContext);
+  return useContext(sayHelloContext);
 }
